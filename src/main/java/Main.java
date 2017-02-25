7a15e97d6a9d3f0bd2f1a3f3b94a00962bd781ee
kronicmage <kronicmage@gmail.com>
2017-02-25 14:52:11 -0500

    Preliminary bot stuff

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,123 @@
+import net.dv8tion.jda.client.entities.Group;
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.entities.*;
+import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.core.exceptions.RateLimitedException;
+import net.dv8tion.jda.core.hooks.ListenerAdapter;
+
+import javax.security.auth.login.LoginException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 /**
  * Created by Simon on 2/25/2017.
  */
-public class Main {
-    //placeholder
+public class Main extends ListenerAdapter {
+    public static void main(String[] args) {
+        /*
+        Your user token should be in the discord.properties file located in the resources folder.
+        Discord doesn't exactly provide an easy way to find your user token, so you'll have to sniff it from your
+        network traffic yourself - here's a guide on how to do just that:
+        https://github.com/Orangestar12/cacobot/wiki/How-to-switch-from-a-standard-user-to-a-Bot-account.
+         */
+        Properties discordProperties = getProperties("discord.properties");
+
+        try {
+            JDA jda = new JDABuilder(AccountType.CLIENT).setToken(discordProperties.getProperty("discord.token")).buildBlocking();
+        } catch (LoginException e) {
+            System.err.println("Token used: " + discordProperties.get("discord.token"));
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (RateLimitedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Gets properties from .properties files
+     *
+     * @param fileName - path and name of properties file
+     * @return A Properties object with all the properties found in the file
+     */
+    private static Properties getProperties(String fileName) {
+        Properties properties = new Properties();
+        try {
+            InputStream in = Main.class.getResourceAsStream("/" + fileName);
+            properties.load(in);
+
+        } catch (IOException e) {
+            System.err.println("There was an error reading " + fileName + ": " + e.getCause()
+                    + " : " + e.getMessage());
+            System.exit(1);
+        }
+
+        return properties;
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        //These are provided with every event in JDA
+        JDA jda = event.getJDA();                       //JDA, the core of the api.
+        long responseNumber = event.getResponseNumber();//The amount of discord events that JDA has received since the last reconnect.
+
+        //Event specific information
+        User author = event.getAuthor();                  //The user that sent the message
+        Message message = event.getMessage();           //The message that was received.
+        MessageChannel channel = event.getChannel();    //This is the MessageChannel that the message was sent to.
+        //  This could be a TextChannel, PrivateChannel, or Group!
+
+        String msg = message.getContent();              //This returns a human readable version of the Message. Similar to
+        // what you would see in the client.
+
+        boolean bot = author.isBot();                     //This boolean is useful to determine if the User that
+        // sent the Message is a BOT or not!
+
+        String debugOutput =
+                "Author: " + author.getName()
+                + "\nMessage: " + msg
+                + "\nIs official bot: " + bot
+                + "\nResponse number: " + responseNumber;
+        System.out.println(debugOutput);
+
+        if (event.isFromType(ChannelType.TEXT))         //If this message was sent to a Guild TextChannel
+        {
+            //Because we now know that this message was sent in a Guild, we can do guild specific things
+            // Note, if you don't check the ChannelType before using these methods, they might return null due
+            // the message possibly not being from a Guild!
+
+            Guild guild = event.getGuild();             //The Guild that this message was sent in. (note, in the API, Guilds are Servers)
+            TextChannel textChannel = event.getTextChannel(); //The TextChannel that this message was sent to.
+            Member member = event.getMember();          //This Member that sent the message. Contains Guild specific information about the User!
+
+            String name = member.getEffectiveName();    //This will either use the Member's nickname if they have one,
+            // otherwise it will default to their username. (User#getName())
+
+            System.out.printf("(%s)[%s]<%s>: %s\n", guild.getName(), textChannel.getName(), name, msg);
+        }
+        else if (event.isFromType(ChannelType.PRIVATE)) //If this message was sent to a PrivateChannel
+        {
+            //The message was sent in a PrivateChannel.
+            //In this example we don't directly use the privateChannel, however, be sure, there are uses for it!
+            PrivateChannel privateChannel = event.getPrivateChannel();
+
+            System.out.printf("[PRIV]<%s>: %s\n", author.getName(), msg);
+        }
+        else if (event.isFromType(ChannelType.GROUP))   //If this message was sent to a Group. This is CLIENT only!
+        {
+            //The message was sent in a Group. It should be noted that Groups are CLIENT only.
+            Group group = event.getGroup();
+            String groupName = group.getName() != null ? group.getName() : "";  //A group name can be null due to it being unnamed.
+
+            System.out.printf("[GRP: %s]<%s>: %s\n", groupName, author.getName(), msg);
+        }
+
+        if (msg.equals("!debug")) {
+
+            channel.sendMessage(debugOutput).queue();
+        }
+    }
 }
